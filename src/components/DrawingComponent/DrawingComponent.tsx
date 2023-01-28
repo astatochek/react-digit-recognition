616202cfacf7557b9124a917557e998d9d512bb2
astatochek <71458498+astatochek@users.noreply.github.com>
2023-01-28 12:44:05 +0300

    add cashing

--- src/components/DrawingComponent/DrawingComponent.tsx
+++ src/components/DrawingComponent/DrawingComponent.tsx
@@ -8,47 +8,185 @@ const Drawing = () => {
     points: number[] = [];
   }
 
+  interface LocalStorageItem {
+    average: number[];
+    num: number;
+  }
+
   const size = 240;
+  const sqSize = 8; // Squeezed Size
+  const labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   const [lines, setLines] = useState<Point[]>([]);
   const isDrawing = useRef(false);
   const stageRef = useRef<Konva.Stage>(null);
   const { num, setNum } = useContext(PredictionContext);
-  
 
   async function getPrediction(data: any) {
 
-    fetch(
-      "https://api-inference.huggingface.co/models/farleyknight/mnist-digit-classification-2022-09-04",
-      {
-        headers: { Authorization: "Bearer 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" },
-        method: "POST",
-        body: data,
-      }
-    ).then(res => {
+    const res = new Promise<string>(resolve => {
+      fetch(
+        "https://api-inference.huggingface.co/models/farleyknight/mnist-digit-classification-2022-09-04",
+        {
+          headers: {
+            Authorization: "Bearer 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",
+          },
+          method: "POST",
+          body: data,
+        }
+      ).then((res) => {
+        if (res.status !== 200) {
+          setTimeout(() => {
+            getPrediction(data);
+          }, 1000);
+        } else {
+          const prediction = res.json().then((body) => {
+            console.log(body);
+            const prediction = body[0].label;
+            // setNum((prev) => prediction);
+            resolve(prediction);
+          });
+        }
+      });
+    })
 
-      if (res.status === 503) {
-        setTimeout(() => {
-          getPrediction(data);
-        }, 20);
-      } else {
-        const prediction = res.json().then(body => {
-          console.log(body);
-          const prediction = body[0].label;
-          setNum(prev => prediction);
-        })
-      }
-
-
-    });
+    return await res;
   }
 
+  function squeeze(data: Uint8ClampedArray): number[] {
+    const matrix: number[][] = [];
+    let buffer: number[] = [];
+    data.forEach((elem, i) => {
+      if (i % 4 === 0) {
+        if (i % (4 * size) === 0) {
+          if (buffer.length !== 0) matrix.push([...buffer]);
+          buffer = [];
+        }
+        buffer.push(elem / 256);
+      }
+    });
+    matrix.push(buffer);
+    buffer = [];
+    // console.log(matrix);
+
+    const step = Math.floor(size / sqSize);
+    console.log("Step:", step);
+
+    // matrix.forEach((line, i) => console.log(i, line));
+
+    const res: number[] = [];
+
+    for (let i = 0; i < sqSize; i++) {
+      for (let j = 0; j < sqSize; j++) {
+        let sum = 0;
+        const rowStart = Math.floor((i * size) / sqSize);
+        const colStart = Math.floor((j * size) / sqSize);
+        for (let k = rowStart; k < rowStart + step; k++) {
+          for (let l = colStart; l < colStart + step; l++) {
+            sum += matrix[k][l];
+          }
+        }
+        // console.log(sum);
+        res.push(sum / (step * step));
+      }
+    }
+
+    // console.log(res);
+
+    return res;
+  }
+
+  function getDistance(a: number[], b: number[]) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+      sum += Math.pow(a[i] - b[i], 2);
+    }
+    return Math.sqrt(sum);
+  }
+
+  function getAverage(input: number[], base: number[], num: number): number[] {
+    const nextAverage: number[] = [];
+    base.forEach((elem, i) => {
+      nextAverage.push((elem + input[i]) / (num + 1))
+    })
+    return nextAverage;
+  }
+
+  function addItemToLocalStorage(input: number[], label: string) {
+    const rawItem = localStorage.getItem(label);
+    if (rawItem) {
+      const item: LocalStorageItem = JSON.parse(rawItem);
+      localStorage.setItem(
+        label,
+        JSON.stringify({
+          average: getAverage(input, item.average, item.num),
+          num: item.num + 1
+        })
+      );
+    } else {
+      localStorage.setItem(
+        label,
+        JSON.stringify({
+          average: input,
+          num: 1,
+        })
+      );
+    }
+  }
+
+  function compareWithCashed(input: number[]): string {
+    const threshold = 0.025;
+    let distances: number[] = [];
+    labels.forEach((label) => {
+      const rawItem = localStorage.getItem(label);
+      if (rawItem) {
+        const item: LocalStorageItem = JSON.parse(rawItem);
+        distances.push(getDistance(input, item.average));
+      } else {
+        distances.push(sqSize);
+      }
+    });
+
+    let sum = 0;
+    distances.forEach(elem => {
+      sum += elem;
+    })
+
+    distances = distances.map(elem => elem / sum);
+
+    const fastPrediction = distances.indexOf(Math.min(...distances));
+
+    console.log('Distances:', distances);
+    console.log('LocalStorage:', localStorage)
+
+    if (distances[fastPrediction] <= threshold) {
+      console.log(distances[fastPrediction], '<=', threshold);
+      return labels[fastPrediction];
+    }
+
+    return "";
+  }
 
   const handleExport = () => {
     if (stageRef.current) {
-      stageRef.current.toBlob().then((res => {
-        getPrediction(res);
-      }))
+      stageRef.current.toBlob().then((res) => {
+        // getPrediction(res);
+        const ctx = stageRef.current?.toCanvas().getContext("2d");
+        if (ctx) {
+          const data = ctx.getImageData(0, 0, size, size).data;
+          const sqData = squeeze(data);
+          const fastPrediction = compareWithCashed(sqData);
+          if (fastPrediction) {
+            setNum(prev => fastPrediction);
+            addItemToLocalStorage(sqData, fastPrediction);
+          } else {
+            getPrediction(res).then(prediction => {
+              setNum(prev => prediction);
+              addItemToLocalStorage(sqData, prediction);
+            })
+          }
+        }
+      });
     }
   };
 
@@ -77,7 +215,6 @@ const Drawing = () => {
   const handleMouseUp = () => {
     isDrawing.current = false;
     handleExport();
-
   };
 
   return (
--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -5,11 +5,34 @@ import PredictionContext from "../Context/PredictionContext";
 const MainComponent = () => {
   const { num, setNum } = useContext(PredictionContext);
 
+  function clearLocalStorage() {
+    localStorage.clear();
+    console.log('LocalStorage:', localStorage);
+  }
+
+  function refreshPage() {
+    window.location.reload();
+  }
+
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full items-center">
       <div className="flex flex-row justify-center m-7">
         <Drawing />
         <div className="border-2 w-60 h-60 text-xl text-center">{num}</div>
+      </div>
+      <div className="flex flex-row justify-center m-7">
+      <button
+          className="w-60 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mx-1"
+          onClick={refreshPage}
+        >
+          Refresh
+        </button>
+        <button
+          className="w-60 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mx-1"
+          onClick={clearLocalStorage}
+        >
+          Clear Cashe
+        </button>
       </div>
     </div>
   );
